7a29278d1946cf3b0515e7a46d915182f48ff689
iwinter74 <isabelle.winternazlisoz@googlemail.com>
2020-10-14 09:58:53 +0200

    Isa v.1

--- index.js
+++ index.js
@@ -10,6 +10,21 @@ const keys = require('./config/keys')
 
 const fetch = require('node-fetch');
 
+// Isabelle
+
+let maxCalories
+let minProtein
+let maxFat
+let maxCarbs
+let maxProtein
+let query_eiweiss
+let query_allergene
+let query_typ
+let query_eigenschaft
+
+//
+
+
 mongoose
      .connect(keys.mongodb.dbURI, { useNewUrlParser: true, useUnifiedTopology: true,useFindAndModify:false })
      .then((result) => {
@@ -40,17 +55,95 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use('/auth', authRoutes);
 app.use('/profile', profileRoutes);
+// app.get('/', (req, res) => {
+//     fetch(
+//          "https://api.spoonacular.com/recipes/complexSearch?type=type&apiKey=84fe766ead804aee905fa97fc4f9ead9"
+//     )
+//     .then((res) => res.json())
+//     .then((json) => res.render('index', { APIData: json.results }));
+// });
+// app.get('/', (req, res) => {
+//     fetch(
+//          "https://api.spoonacular.com/recipes/complexSearch?type=type&apiKey=84fe766ead804aee905fa97fc4f9ead9"
+//     )
+//     .then((res) => res.json())
+//     .then((json) => res.render('index', { APIData2: json.results }));
+// });
+
+// Isabelle
+
+// app.get('/', (req, res) => {
+//      fetch(`https://api.spoonacular.com/recipes/findByNutrients?maxCalories=${maxCalories}&minProtein=${minProtein}&maxFat=${maxFat}&maxCarbs=${maxCarbs}&maxProtein=${maxProtein}&number=10&apiKey=${process.env.apiKey}`)
+//          .then(res => res.json())
+//          .then(json => {
+//               data = json;
+//               console.log(data)
+//           //     res.send(data)
+//              res.status(200).render('index', {  })
+//          })
+//          .catch(err => console.log(err))
+// })
+
+// fetch(`https://api.spoonacular.com/food/search?query=${query_eiweiss} ${query_allergene} ${query_typ} ${query_eigenschaft}&number=2&apiKey=${process.env.apiKey}`)
+
 app.get('/', (req, res) => {
-    fetch(
-         "https://api.spoonacular.com/recipes/complexSearch?type=type&apiKey=84fe766ead804aee905fa97fc4f9ead9"
-    )
-    .then((res) => res.json())
-    .then((json) => res.render('index', { APIData: json.results }));
-});
-app.get('/', (req, res) => {
-    fetch(
-         "https://api.spoonacular.com/recipes/complexSearch?type=type&apiKey=84fe766ead804aee905fa97fc4f9ead9"
-    )
-    .then((res) => res.json())
-    .then((json) => res.render('index', { APIData2: json.results }));
-});
+     fetch(`https://api.spoonacular.com/food/search?query=${query_eiweiss}&number=2&apiKey=${process.env.apiKey}`)
+         .then(res => res.json())
+         .then(json => {
+              data = json;
+              console.log(data)
+          //     res.send(data)
+             res.status(200).render('index', {  })
+         })
+         .catch(err => console.log(err))
+})
+
+
+app.post("/filter", (req, res) => {
+     console.log(req.body)
+     if (req.body.ziel=="abnehmen") {
+          maxCalories = 700
+     } else {
+          maxCalories=3000
+     }
+     if (req.body.ziel=="muskel") {
+          minProtein = 25
+     } else {
+          minProtein = 0
+     }
+     if (req.body.ziel=="gesund") {
+          maxFat = 5;
+          maxCarbs = 30;
+          maxProtein = 3;
+          maxCalories = 1000
+     } else {
+          maxFat = 50;
+          maxCarbs = 300;
+          maxProtein = 50;
+          maxCalories = 3000
+     }
+     query_eiweiss = req.body.eiweiss
+     query_allergene = req.body.allergene
+     query_typ = req.body.typ
+     query_eigenschaft = req.body.eigenschaft
+     
+
+     console.log("maxCalories: " + maxCalories, "minProtein: " + minProtein, "maxFat: " + maxFat, "maxCarbs: " + maxCarbs, "maxProtein: " + maxProtein)
+     console.log("eiweiss: " + query_eiweiss)
+     console.log("allergene: " + query_allergene)
+     console.log("typ: " + query_typ)
+     console.log("eingenschaft: " + query_eigenschaft)
+
+
+     res.redirect("/")
+})
+
+// min protein muskelaufbau: 25g
+// max calories abnehmen: 700 cal
+// protein cal carb fat gesund leben: fat max. 5g, carb max. 30g, protein max. 3g, cal max. 1000 cal
+
+// diet parameter: vegan
+
+
+
+ 
